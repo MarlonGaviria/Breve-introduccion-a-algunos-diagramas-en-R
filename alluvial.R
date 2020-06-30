ec9766b454435a1fb2636314d9588715cb067570
MarlonG <marlongaviriaz@gmail.com>
2020-06-30 18:36:04 -0500

    Este es el codigo para hacer los graficos con el paquete alluvial

---
+++ alluvial.R
@@ -0,0 +1,14 @@
+# install.packages(c("alluvial"), dependencies = TRUE)
+require(alluvial)
+
+# Titanic data
+tit <- as.data.frame(Titanic)
+
+# 4d
+alluvial( tit[,1:4], freq=tit$Freq,
+          hide = tit$Freq < quantile(tit$Freq, .50),
+          col=ifelse( tit$Class == "3rd" & tit$Sex == "Male", "red", "gray"),
+          border = 'gray')
+
+
+
